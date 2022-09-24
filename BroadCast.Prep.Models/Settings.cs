9bd518d89e4085e3a94091d5248a2b8bff32d920
Ben Osborne <osborne.ben@gmail.com>
2022-09-24 10:10:24 -0500

    refactoring

--- BroadCast.Prep.Models/Settings.cs
+++ BroadCast.Prep.Models/Settings.cs
@@ -11,19 +11,10 @@ public record Settings
     public string PagesDestinationFolder { get; init; } = default!;
 
     [Required(AllowEmptyStrings = false)]
-    public string VideoBackgroundSourceFolder { get; init; } = default!;
-
-    [Required(AllowEmptyStrings = false)]
-    public string VideoBackgroundDestinationPath { get; init; } = default!;
-
-    [Required(AllowEmptyStrings = false)]
     public string DateTxtPath { get; init; } = default!;
 
     [Required(AllowEmptyStrings = false)]
     public string TitleAndDescriptionTxtPath { get; init; } = default!;
-
-    [Required(AllowEmptyStrings = false)]
-    public string RawTxtPath { get; init; } = default!;
 
     [Required(AllowEmptyStrings = false)]
     public string TitleAndDescriptionTemplate { get; init; } = default!;
