d6b9665db2276e73fde9e6ff095685ecd9fc8d95
Ben Osborne <ben@osbornesupremacy.com>
2024-08-04 07:30:04 -0500

    fixed type

--- BroadCast.Prep.Service/SermonExportService.cs
+++ BroadCast.Prep.Service/SermonExportService.cs
@@ -100,7 +100,7 @@ public static class SermonExportService
                         
                         {sermon.Passage}. "{sermon.Title}"
 
-                        Port URL:
+                        Post URL:
 
                         {sermon.ToUrl()}
 
